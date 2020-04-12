33c77247bb159cf49aee32f68a3e40ea7b7e7c7d
NoumanMasood <noumanmasood64@gmail.com>
2020-04-12 18:44:07 +0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const Hatspage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+      <Route exact path='/' component={Homepage}></Route>
+      <Route path='/shop/hats' component={Hatspage}></Route>
+      </Switch>
     </div>
   );
 }
